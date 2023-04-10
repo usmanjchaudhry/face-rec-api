5d984f9ac68365f068d9caed1179840efc6ebf62
usmanjchaudhry <uidman72@gmail.com>
2023-04-10 07:11:56 -0700

    Moved it

---
+++ server.js
@@ -0,0 +1,148 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors');
+const { dblClick } = require('@testing-library/user-event/dist/click');
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : '',
+      database : 'face-brain'
+    }
+  });
+  
+
+
+
+knex.select('*').from('users').then(data =>{
+    console.log(data)
+})
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+const database = {
+    users:[
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password:'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password:'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+    ]
+}
+
+
+
+
+app.get('/', (req, res)=>{
+    res.send(database.users)
+}) 
+
+app.post('/signin',(req,res)=>{
+    const {email, password} = req.body;
+    if(!email || !password){
+        res.status(400).json('incorrect form submission')
+    }
+knex.select('email','hash').from('login')
+.where('email', '=', req.body.email)
+    .then(data =>{
+       const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
+       console.log(isValid)
+       if(isValid){
+       return knex.select('*').from('users')
+        .where('email', '=', req.body.email)
+        .then(user =>{
+            res.json(user[0])
+        })
+        .catch(err => res.status(400).json('unable to get user'))
+       }else{
+       res.status(400).json('wrong credentials')
+       }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
+})
+
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    if(!email || !name || !password){
+        res.status(400).json('incorrect form submission')
+    }
+    const hash = bcrypt.hashSync(password);
+  
+    knex.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login') 
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0].email,
+            name: name,
+            joined: new Date()
+          })
+      .then(user => {
+        res.json(user[0]);
+      })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
+})
+      .catch(err => {
+        res.status(400).json('unable to register');
+      });
+    });
+
+
+app.get('/profile/:id', (req,res)=>{
+    const { id } = req.params;
+    knex.select('*').from('users').where({id})
+    .then(user =>{
+        if(user.length){
+            res.json(user[0])
+        }else{
+            res.status(400).json('Not found')
+        }
+    })
+    .catch(err => res.status(400).json('error getting user'))
+})
+  
+
+
+    app.put('/image', (req, res) => {
+        const { id } = req.body;
+        knex('users')
+          .where('id', '=', id)
+          .increment('entries', 1)
+          .returning('entries')
+          .then(entries => {
+            res.json(entries[0]);
+          })
+          .catch(err => res.status(400).json('unable to get count'));
+      });
+      
+
+
+
+app.listen( process.env.PORT || 3000, ()=>{
+    console.log(`App is running on port ${process.env.PORT}`)
+})
